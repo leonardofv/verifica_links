8025d36685f69236f65acfd56f859722835edf52
Leonardo Fonteles <fonteles.leonardo@gmail.com>
2023-09-19 18:01:55 -0300

    verificando se o caminho é um arquivo ou diretório

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,26 @@
 import pegaArquivo from "./index.js";
 import chalk from "chalk";
+import fs from 'fs';
 
 const caminho = process.argv;
 
-async function processaTexto(caminho) {
-    const result = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('Links'), result);
+function imprime(result) {
+    console.log(chalk.yellow('Lista de Links'), result);
+};
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+    //verificando se é arquivo ou diretório com fs
+    if (fs.lstatSync(caminho).isFile()) {
+        const result = await pegaArquivo(argumentos[2]);
+        imprime(result);
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho); //ler diretório
+        arquivos.forEach(async (nomeDoArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDoArquivo}`)
+            imprime(lista);
+        });
+    };
 };
 
 processaTexto(caminho);
